b7650af34520edee5d36e1840fef6962654ff4b6
Simon <osipov.simon@gmail.com>
2017-09-17 00:14:11 +0300

    SHOW / HELP functionality added

--- shopping_list.py
+++ shopping_list.py
@@ -1,22 +1,39 @@
+# Function to show help for user
+def show_help():
+    print("Type DONE to finish")
+    print("Type SHOW to show current list")
+    print("Type HELP to display help")
+
+# Printing out all items on the list
+def show_list():
+    print("Here is the full list:")
+    for item in shopping_list:
+        print(item)
+
 # Creating empty shopping list
 shopping_list = []
 
-# Print instructions for user
-print("What should i pick from store?")
-print("Type DONE to finish")
+# Show initial HELP message
+show_help()
 
 # Entering new items
 while True:
     new_item = input("> ")  # Ask for new items
 
+    # Process "DONE" request
     if new_item == "DONE":
         break
 
-    # Add new items to the list
-    shopping_list.append(new_item)
+    # Process "HELP" request
+    elif new_item == "HELP":
+        show_help()
+        continue
 
-# Printing out all items on the list
-print("Here is the full list:")
-for item in shopping_list:
-    print(item)
+    # Process "SHOW" request
+    elif new_item == "SHOW":
+        show_list()
+
+    shopping_list.append(new_item) # Add new items to the list
+
+
 
