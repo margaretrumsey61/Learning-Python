2021f8d78a4012dfcb7346ceebb23fc174140526
Simon <osipov.simon@gmail.com>
2017-09-17 13:49:26 +0300

    Comments added

--- age_calc.py
+++ age_calc.py
@@ -1,27 +1,32 @@
-# Step 1
 # Ask the user for their name and the year they were born.
 try:
     date_of_birth = int(input("Hey, Player, what is your date of birth \n"))
+
+# Handling ValueError if user enters not a number
 except ValueError:
     print("No-no-no, years only!")
 else:
+    # Calculating current age
     current_age = 2017 - date_of_birth
+
+    # Handle age <= 25
     if current_age <= 25:
         print("You will turn 25 in {}".format(date_of_birth + 25))
         print("You will turn 55 in {}".format(date_of_birth + 55))
         print("You will turn 75 in {}".format(date_of_birth + 75))
         print("You will turn 100 in {}".format(date_of_birth + 100))
+
+    # Handle age <=50
     elif current_age <= 50:
         print("You will turn 55 in {}".format(date_of_birth + 55))
         print("You will turn 75 in {}".format(date_of_birth + 75))
         print("You will turn 100 in {}".format(date_of_birth + 100))
+
+    # Handle age <=75
     elif current_age <= 75:
         print("You will turn 75 in {}".format(date_of_birth + 75))
         print("You will turn 100 in {}".format(date_of_birth + 100))
+
+    # Handle age <=100
     elif current_age <= 100:
         print("You will turn 100 in {}".format(date_of_birth + 100))
-# Step 2
-# Calculate and print the year they'll turn 25, 50, 75, and 100.
-
-# Step 3
-# If they're already past any of these ages, skip them.
