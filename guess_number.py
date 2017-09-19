45738fe0ee18ffb34d5e20099e49f5ead4b942f4
Simon <osipov.simon@gmail.com>
2017-09-19 18:15:54 +0300

    New function added
    show_help function. Explain to user his task

--- guess_number.py
+++ guess_number.py
@@ -5,7 +5,15 @@ def generate_secret_number():
     return random.randint(1, 10)
 
 
+def show_help():
+    print("Hi, my name is Mr. Robot!\n"
+          "I want you to play my game.\n"
+          "You have 5 attempts to guess what number i picked for you (from 1 to 10 \n")
+
+
 secret_number = generate_secret_number()
+
+show_help()
 
 while True:
     players_guess = int(input("Please, guess secret number from 1 to 10: "))
