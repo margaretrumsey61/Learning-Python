ce5d602f3b80d0fe7b25cd8866eb9f4ceedd7f9e
Simon <osipov.simon@gmail.com>
2017-09-19 19:21:09 +0300

    Replay functionality added
    You can play again after you finish the game

--- guess_number.py
+++ guess_number.py
@@ -4,7 +4,7 @@ import random
 def show_help():
     print("Hi, my name is Mr. Robot!\n"
           "I want you to play my game.\n"
-          "You have 5 attempts to guess what number i picked for you (from 1 to 10 \n")
+          "You have 5 attempts to guess what number i picked for you (from 1 to 10) \n")
 
 
 def new_game():
@@ -16,19 +16,24 @@ def new_game():
         try:
             players_guess = int(players_guess)
         except ValueError:
-            print("I need number, man! Number! '{}' is not the number".format(players_guess))
+            print("I need number, man! Number! '{}' is not the number \n".format(players_guess))
         else:
             if players_guess == secret_number:
                 print("Hooray, you guessed right!")
                 break
             elif players_guess > secret_number:
-                print("Not, my secret number is less than {}".format(players_guess))
+                print("Not, my secret number is less than {} \n".format(players_guess))
             else:
-                print("Not, my secret number is higher than {}".format(players_guess))
+                print("Not, my secret number is higher than {} \n".format(players_guess))
 
             guesses.append(players_guess)
     else:
-        print("You ran out of guesses. {} was the right answer".format(secret_number))
+        print("You ran out of guesses. {} was the right answer \n".format(secret_number))
+    play_again = input("Do you want to play again? Y/n \n")
+    if play_again != 'n':
+        new_game()
+    else:
+        print("Bye!")
 
 
 new_game()
