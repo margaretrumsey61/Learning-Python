d6bdcd0bad1ba3ef41e817b4ca5cd240db431b37
Simon <osipov.simon@gmail.com>
2017-09-19 19:13:45 +0300

    Proper error handling established

--- guess_number.py
+++ guess_number.py
@@ -5,28 +5,28 @@ def show_help():
     print("Hi, my name is Mr. Robot!\n"
           "I want you to play my game.\n"
           "You have 5 attempts to guess what number i picked for you (from 1 to 10 \n")
-    
+
 
 def new_game():
     show_help()
     secret_number = random.randint(1, 10)
     guesses = []
     while len(guesses) < 5:
+        players_guess = input("Please, guess secret number from 1 to 10: ")
         try:
-            players_guess = int(input("Please, guess secret number from 1 to 10: "))
+            players_guess = int(players_guess)
         except ValueError:
-            print("I need number, man! Number!")
+            print("I need number, man! Number! '{}' is not the number".format(players_guess))
         else:
             if players_guess == secret_number:
-                print("Hoorah, you guessed right!")
+                print("Hooray, you guessed right!")
                 break
-            elif check_for_delta():
-                print("Way too far away!")
             else:
                 print("Not, my secret number is not {}".format(players_guess))
 
             guesses.append(players_guess)
     else:
         print("You ran out of guesses. {} was the right answer".format(secret_number))
+
 
 new_game()
