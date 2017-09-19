f8735f47a84509bdcb03dfbe3f60cbb5ce3ee2b6
Simon <osipov.simon@gmail.com>
2017-09-19 18:46:32 +0300

    Too far away function deleted
    + limit of guesses implemented

--- guess_number.py
+++ guess_number.py
@@ -1,36 +1,32 @@
 import random
 
 
-def generate_secret_number():
-    return random.randint(1, 10)
-
-
 def show_help():
     print("Hi, my name is Mr. Robot!\n"
           "I want you to play my game.\n"
           "You have 5 attempts to guess what number i picked for you (from 1 to 10 \n")
+    
 
-
-def check_for_delta():
-    if players_guess - secret_number >= 3 or players_guess - secret_number <= - 3:
-        return True
-
-
-secret_number = generate_secret_number()
-
-show_help()
-
-while True:
-    try:
-        players_guess = int(input("Please, guess secret number from 1 to 10: "))
-    except ValueError:
-        print("I need number, man! Number!")
-    else:
-        if players_guess == secret_number:
-            print("Hoorah, you guessed right!")
-            break
-        elif check_for_delta():
-            print("Way too far away!")
+def new_game():
+    show_help()
+    secret_number = random.randint(1, 10)
+    guesses = []
+    while len(guesses) < 5:
+        try:
+            players_guess = int(input("Please, guess secret number from 1 to 10: "))
+        except ValueError:
+            print("I need number, man! Number!")
         else:
-            print("Not, my secret number is not {}".format(players_guess))
+            if players_guess == secret_number:
+                print("Hoorah, you guessed right!")
+                break
+            elif check_for_delta():
+                print("Way too far away!")
+            else:
+                print("Not, my secret number is not {}".format(players_guess))
 
+            guesses.append(players_guess)
+    else:
+        print("You ran out of guesses. {} was the right answer".format(secret_number))
+
+new_game()
